4afe2bc170979cede35719b68be59fd044138075
Jeff Rasley <jeffra45@gmail.com>
2024-04-25 17:37:29 -0700

    fixes

--- src/transformers/models/arctic/configuration_arctic.py
+++ src/transformers/models/arctic/configuration_arctic.py
@@ -16,8 +16,8 @@
 from dataclasses import asdict, dataclass
 from typing import Any, Dict
 
-from ...configuration_utils import PretrainedConfig
-from ...utils import logging
+from transformers.configuration_utils import PretrainedConfig
+from transformers.utils import logging
 
 
 logger = logging.get_logger(__name__)
